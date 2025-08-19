3f123f225085c837e781bd0b978b77693a7de597
angerypr <angerypr16@gmail.com>
2025-08-19 11:35:30 -0400

    Carpeta reportero

---
+++ reportero/panel.php
@@ -0,0 +1,5 @@
+<p>
+            Bienvenido a <strong>Incident Hub</strong>, la plataforma para registrar,
+            visualizar y gestionar incidencias ocurridas en el país.
+            Selecciona tu rol para continuar.
+        </p>
